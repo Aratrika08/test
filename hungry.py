296446eb2715f0478080650c7e7555ea4e9c4260
Aratrika08 <kunduaratrika@gmail.com>
2021-01-14 23:27:05 +0530

    removed burger

--- hungry.py
+++ hungry.py
@@ -2,7 +2,6 @@ hungry=input("are you hungry?")
 if hungry=="yes":
     print("Eat samosa")
     print("Eat fries")
-    print("Eat burger")
 else:
     thirsty=input("are you thirsty?")
     if thirsty=="yes":
